95cf51d30cc1c2aa906dbacc55bbe9c237d13808
zeid zein <zeid.zen@gmail.com>
2020-02-08 22:07:28 +0200

    DB Processing

---
+++ processes_DB.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 19:48:16 2019
+
+@author: zeidz
+"""
+import database as db
+import hashlib, binascii
+import re
+import os 
+from nltk import sent_tokenize
+
+
+
+class show_data () : 
+    
+    def __init__(self) :
+        self.con=db.DataBase()
+      
+
+
+class insert_data () : 
+    
+    def __init__ (self) :
+        self.con=db.DataBase()
+    
+
+    
+class Register_And_login () : 
+    
+    def __init__(self) :
+         self.con=db.DataBase()
+         
