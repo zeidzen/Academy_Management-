53b8fee2d20027e916291fa5d55054cc564e44fd
semo4 <34076117+semo4@users.noreply.github.com>
2020-02-14 02:07:23 +0200

    new pages

--- pages.py
+++ pages.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from flask import Flask, request, jsonify
+import processes_DB
+from flask import session
 import processes_DB
 
 
@@ -13,19 +16,18 @@ class Header():
         self.data['courses_categories'] = self.show_data.get_all_categories_course()
         self.data['courses'] = self.show_data.get_all_courses()
         self.data['courses_name'] = self.show_data.get_courses_name()
+        self.data['categories'] = self.show_data.get_all_categories_item()
 
     
 class Footer():
     pass
 
 
-class Home( Header ):
-    def __init__(self) :
+class Home(Header):
+    def __init(self):
         super().__init__()
-        
-        self.data ['offers_Of_courses'] = self.show_data.get_offer_by_courses()
-        self.data ['offers_Of_produts']= self.show_data.get_offer_by_produts()
-
+        self.data['offers_Of_courses'] = self.show_data.get_offer_by_courses()
+        self.data['offers_Of_produts'] = self.show_data.get_offer_by_produts()
 
 
 class Courses(Header):
@@ -56,10 +58,30 @@ class Sinup(Header):
         super().__init__()
         self.data['title'] = 'Sinup'
         self.data['Cities'] = self.show_data.get_all_cities()
-    def Regiter(self , **info):
+
+    def Regiter(self, **info):
         self.insert_data = processes_DB.Register_And_login()
         self.insert_data.Register_func(**info)
 
+
+class Add(Header):
+    def __init__(self):
+        super().__init__()
+        self.data['title'] = 'Add Category'
+
+    def Add_category(self, **info):
+        self.insert_data = processes_DB.insert_data()
+        self.insert_data.add_category(**info)
+
+
+class Add_Items(Header):
+    def __init__(self):
+        super().__init__()
+        self.data['title'] = 'Add Items'
+
+    def Add_items(self, **info):
+        self.insert_data = processes_DB.insert_data()
+        self.insert_data.add_items(**info)
 
 
 class About(Header):
