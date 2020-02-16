ef2859165a332f0f2729ec031f5e6eeeed657b63
Osama yousef <fotboy1788@hotmail.com>
2020-02-16 06:04:36 +0200

    add contact

--- pages.py
+++ pages.py
@@ -22,6 +22,11 @@ class Header():
         self.data['courses_name'] = self.show_data.get_courses_name()
         self.data['categories'] = self.show_data.get_all_categories_item()
 
+    def Check_Image_Extenstion(self,filename):
+        ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # Footer class
 class Footer():
@@ -30,7 +35,7 @@ class Footer():
 
 # Home class
 class Home(Header):
-    def __init(self):
+    def __init__(self):
         super().__init__()
         self.data['offers_Of_courses'] = self.show_data.get_offer_by_courses()
         self.data['offers_Of_produts'] = self.show_data.get_offer_by_produts()
@@ -60,7 +65,7 @@ class Courses(Header):
 class Products(Header):
     def __init__(self , page ):
         super().__init__()
-        self.data ['all_products'] = self.show_data.get_all_items()
+        self.data ['all_products'] = self.show_data.get_all_products()
         self.data ['Most_Watched'] = self.show_data.get_top_viewed_item()
         self.data ['page'] = int(page)
         self.data ['Max_page'] = math.ceil(len(self.data['all_products'])/9)
@@ -141,7 +146,7 @@ class Login(Header):
         super().__init__()
         self.data['title'] = 'Login'
     def get_login(self, **info):
-        self.register_user.Login_func(info)
+        return self.register_user.Login_func(info)
 
 
 # Achievements class
@@ -190,7 +195,7 @@ class Signup(Header):
         self.data['Cities'] = self.show_data.get_all_cities()
 
     def Regiter(self, **info):
-        self.register_user.Register_func(**info)
+        return self.register_user.Register_func(**info)
 
 
 # category class
@@ -255,6 +260,13 @@ class About(Header):
     def __init__(self):
         super().__init__()
         self.data['title'] = 'About Us'
+
+
+# Content class
+class Contact(Header):
+    def __init__(self):
+        super().__init__()
+        self.data['title'] = 'Content'
 
 
 # FAQ class
