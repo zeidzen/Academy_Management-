d3ecf4b67c1e30fc722a4c69361b9412037d5717
zeid zein <zeid.zen@gmail.com>
2020-02-29 20:25:53 +0200

    news

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ port=5000
 host_DB ='127.0.0.1'
 host_app ='127.0.0.1' # '62.171.142.203'
 username='root'  # 'tta'
-password='' # '$T20t_20A$'
+password='2020' # '$T20t_20A$'
 database='tabasheer_academy'
 
 #gonicorn Ip : 0.0.0.0
